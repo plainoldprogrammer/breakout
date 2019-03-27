5ae44b251c74d032b83091d741a95c17647686ff
plainoldprogrammer <plainoldprogrammer@gmail.com>
2019-03-27 01:38:36 -0600

    creating a basic bar

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Breakout videogame!";
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RectangleShape bar;
+    bar.setSize(sf::Vector2f(100, 20));
+    bar.setOutlineColor(sf::Color::Red);
+    bar.setOutlineThickness(3);
+    bar.setPosition(10, 20);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(bar);
+        window.display();
+    }
 
     return 0;
 }
